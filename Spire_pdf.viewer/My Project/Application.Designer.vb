84fedc4d61d8b3afcedb3d5e9f3c281499fcb721
sjyogi73 <sjyogi73@gmail.com>
2023-04-10 11:20:09 +0530

    Added License Xml File

--- Spire_pdf.viewer/My Project/Application.Designer.vb
+++ Spire_pdf.viewer/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Spire_pdf.viewer.Document
+            Me.MainForm = Global.Spire_pdf.viewer.Viewer
         End Sub
     End Class
 End Namespace
--- Spire_pdf.viewer/Viewer.Designer.vb
+++ Spire_pdf.viewer/Viewer.Designer.vb
@@ -20,26 +20,41 @@ Partial Class Viewer
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+
+        'Initialize Spire License Key
+        'Spire.License.LicenseProvider.SetLicenseFile(licenseFile)
+
+        Spire.License.LicenseProvider.SetLicenseKey(key)
+
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.PdfViewer1 = New Spire.PdfViewer.Forms.PdfViewer()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
         Me.Panel1.Controls.Add(Me.Button2)
-        Me.Panel1.Controls.Add(Me.Button1)
         Me.Panel1.Controls.Add(Me.PdfViewer1)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1084, 536)
+        Me.Panel1.Size = New System.Drawing.Size(1220, 670)
         Me.Panel1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1009, 52)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(9, 10)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'PdfViewer1
         '
@@ -50,11 +65,12 @@ Partial Class Viewer
         Me.PdfViewer1.FormFillEnabled = False
         Me.PdfViewer1.IgnoreCase = False
         Me.PdfViewer1.IsToolBarVisible = True
-        Me.PdfViewer1.Location = New System.Drawing.Point(0, 42)
+        Me.PdfViewer1.Location = New System.Drawing.Point(0, 4)
+        Me.PdfViewer1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.PdfViewer1.MultiPagesThreshold = 60
         Me.PdfViewer1.Name = "PdfViewer1"
         Me.PdfViewer1.OnRenderPageExceptionEvent = Nothing
-        Me.PdfViewer1.Size = New System.Drawing.Size(1081, 491)
+        Me.PdfViewer1.Size = New System.Drawing.Size(1216, 662)
         Me.PdfViewer1.TabIndex = 0
         Me.PdfViewer1.Text = "PdfViewer1"
         Me.PdfViewer1.Threshold = 60
@@ -64,37 +80,15 @@ Partial Class Viewer
         '
         Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
-        'Button1
-        '
-        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Button1.BackColor = System.Drawing.Color.DodgerBlue
-        Me.Button1.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button1.Location = New System.Drawing.Point(958, 0)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(123, 36)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Load pdf"
-        Me.Button1.UseVisualStyleBackColor = False
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(897, 42)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(8, 8)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
         'Viewer
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1084, 536)
+        Me.ClientSize = New System.Drawing.Size(1220, 670)
         Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Viewer"
-        Me.Text = "Form1"
+        Me.Text = "Spire License Evaluation"
         Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -103,6 +97,5 @@ Partial Class Viewer
     Friend WithEvents Panel1 As Panel
     Friend WithEvents PdfViewer1 As Spire.PdfViewer.Forms.PdfViewer
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
-    Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
 End Class
